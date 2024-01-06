64016081ad5f288fb754348205d2985bd738eeac
Ceren <cerengunnes@gmail.com>
2024-01-07 02:53:09 +0300

    Add custom alert.

--- GHFollowers/GHFollowers/SearchVC.swift
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -40,7 +40,7 @@ class SearchVC: UIViewController {
     
     @objc func pushFollowerListVC() {
         guard isUsernameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username, we need to know who to look for🤗", buttonTitle: "Ok")
             return
         }
         
---
+++ GHFollowers/GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Ceren Güneş on 7.01.2024.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
