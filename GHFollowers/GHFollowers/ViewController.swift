72b4fbea441c0b3a1dfd26063c88631d1a86af59
Ceren <cerengunnes@gmail.com>
2024-01-06 21:50:15 +0300

    Add assets and removed storyboard.

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Ceren Güneş on 6.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
